dfa5fa46ab49536547d42dae22432b029adc0da4
bswole <smk1986@yahoo.com>
2021-02-05 16:40:32 -0700

    logout bp added

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -61,7 +61,6 @@ def login():
     
   return render_template('auth/login.html')
 
-  
 @bp.before_app_first_request
 def load_logged_in_user():
   user_id = session.get('user_id')
@@ -72,3 +71,8 @@ def load_logged_in_user():
     g.user = get_db().execute(
       'SELECT * FROM user WHERE id = ?', (user_id,)
     ).fetchone()
+
+@bp.route('/logout')
+def logout():
+  session.clear()
+  return redirect(url_for('index'))
